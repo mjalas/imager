63862eddf997344f79969cbb7d19b3aee6d4f15d
Mats Jalas <mats.jalas@gmail.com>
2022-12-14 20:20:20 +0200

    Improve convert_to_rgb command

--- imager/main.py
+++ imager/main.py
@@ -6,18 +6,20 @@ app = typer.Typer()
 
 
 @app.command()
-def convert_to_rgb(input_image_file: str, output_name: str = "output", show_only: bool = False):
+def convert_to_rgb(input_image_file: str, output_name: str = "output", show_only: bool = False, jpeg: bool = False):
         input_name_parts = input_image_file.split('.')
-        output_name_file = f"{output_name}.{input_name_parts[1]}"
+        
         with Image.open(input_image_file) as image:
             if image.mode == 'RGBA':
-                output_image = Image.new("RGBA", image.size, "WHITE")
-                output_image.paste(image, (0, 0), image)
-                output_image.convert("RGB")
+                white_image = Image.new("RGB", image.size, "WHITE")
+                white_image.paste(image, (0, 0), image)
+                output_image = white_image.convert("RGB")
                 if show_only:
                     output_image.show()
                 else:
-                    output_image.save(output_name_file, input_name_parts[1].upper())
+                    file_type = 'jpeg' if jpeg else input_name_parts[1]
+                    output_name_file = f"{output_name}.{file_type}"
+                    output_image.save(output_name_file, file_type)
 
             else:
                 print(f'cannot yet handle image in mode {image.mode}')
